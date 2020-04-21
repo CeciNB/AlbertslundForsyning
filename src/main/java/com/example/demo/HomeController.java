649170a3877fcaf9a96ce33a8120f1bc12f663fc
ceci138p <ceci138p@stud.kea.dk>
2020-04-21 10:12:30 +0200

    html flies

---
+++ src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping ("/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping ("/fjernvarme")
+    public String fjernvarme(){
+        return "fjernvarme";
+    }
+    @GetMapping ("/energibesparelse")
+    public String energibesparelse(){
+        return "energibesparelse";
+    }
+    @GetMapping ("/dritstatus")
+    public String dritstatus(){
+        return "dritstatus";
+    }
+}
